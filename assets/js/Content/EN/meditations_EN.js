8c7e1f12028cd496bf558912ee0983ce31e92a41
Arthur0025 <2051991099@qq.com>
2026-04-27 11:58:19 +0800

    update summer love

--- assets/js/Content/EN/meditations_EN.js
+++ assets/js/Content/EN/meditations_EN.js
@@ -5,9 +5,56 @@
   <div id="meditations">
     <div class="container">
       <div class="resume-heading">Meditations</div>
+        <h2>Summer Love</h2>
+        <div class="medit-poem">
+              <div class="stanza">
+                <p>The scorching wind blows through the midsummer</p>
+                <p>Bright sunlight filters through the dappled tree shadows</p>
+                <p>I reach out to catch the noisy cicada songs</p>
+                <p>Standing beneath the shade, waiting for my beloved to come</p>
+              </div>
 
-      <div class="section">
-        <div class="medit-contents">
+              <div class="stanza">
+                <p>The melting ice cream drips drip drip drip</p>
+                <p>We share the plum soup in the white porcelain bowl</p>
+                <p>The split chilled watermelon holds boundless love</p>
+                <p>And the girl's eyes reflected in its red flesh</p>
+              </div>
+
+              <div class="stanza">
+                <p>Footsteps echo through the town on the orange-scented breeze</p>
+                <p>Frogs in the pond tell of our sweetness</p>
+                <p>Lotus flowers in the park witness our vows</p>
+                <p>I long to hold your hand and turn into birds to travel the world</p>
+              </div>
+
+              <div class="stanza">
+                <p>To see the sky embrace the sea</p>
+                <p>To hear seagulls murmur in the wind</p>
+                <p>And to walk side by side on the Milky Way</p>
+                <p>Hiding our forms</p>
+                <p>Eavesdropping on the whispers of the Cowherd and Weaver Girl</p>
+              </div>
+
+              <div class="stanza">
+                <p>Or lost in the sweetness of day after day</p>
+                <p>The start of school comes unexpectedly</p>
+                <p>We have to scatter like dandelion seeds</p>
+                <p>One here, one there</p>
+              </div>
+
+              <div class="stanza">
+                <p>The distant distance</p>
+                <p>Can never defeat the love that spans the entire summer</p>
+              </div>
+
+              <div class="stanza">
+                <p>Looking back on the whole midsummer</p>
+                <p>It is full of your warm palm</p>
+                <p>And the endless cicada songs</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- assets/js/Content/ZH/meditations_ZH.js
+++ assets/js/Content/ZH/meditations_ZH.js
@@ -5,6 +5,57 @@
 
   <div class="section">
     <div class="medit-contents">
+      <div class="article">
+        <h2>盛夏的小爱情</h2>
+        <div class="medit-poem">
+          <div class="stanza">
+            <p>炽热的风吹过盛夏</p>
+            <p>明媚的阳光透过树的碎影</p>
+            <p>伸手抓住聒噪的蝉鸣</p>
+            <p>树荫下伫立 等待心上人的来临</p>
+          </div>
+
+          <div class="stanza">
+            <p>融化的冰激淋哒嘀哒嘀</p>
+            <p>白瓷碗中的梅子汤两人共饮</p>
+            <p>剖开的冰镇西瓜藏着无限爱意</p>
+            <p>和那映着女孩的眼睛</p>
+          </div>
+
+          <div class="stanza">
+            <p>脚步声顺着橘子味的风响彻小镇</p>
+            <p>池塘里的青蛙诉说着我们的甜蜜</p>
+            <p>公园里的荷花见证着我们的誓言</p>
+            <p>好想牵着你的手化作飞鸟去往世界各地</p>
+          </div>
+
+          <div class="stanza">
+            <p>看天空与大海拥抱</p>
+            <p>听海鸥在风中呢喃</p>
+            <p>还想并肩去银河上走一趟</p>
+            <p>隐匿身形</p>
+            <p>偷听牛郎织女的低语</p>
+          </div>
+
+          <div class="stanza">
+            <p>或是陷入了日复一日的甜蜜</p>
+            <p>开学日期猝不及防地来临</p>
+            <p>只好如蒲公英四散分离</p>
+            <p>一人在此 一人在彼</p>
+          </div>
+
+          <div class="stanza">
+            <p>遥远的距离</p>
+            <p>怎能敌过横亘整个槐序的爱情</p>
+          </div>
+
+          <div class="stanza">
+            <p>回忆起整个盛夏</p>
+            <p>满满的都是你温暖的手心</p>
+            <p>和那不绝于耳的蝉鸣</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </div>
